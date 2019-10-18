61f4ba79f90001d6a6d889a98c7d062d78b7354b
Kevin J Donohue <kevinjdonohue@gmail.com>
2019-10-18 16:19:57 -0700

    Adding exercises 18 - 21

--- ex17.py
+++ ex17.py
@@ -1,23 +1,27 @@
 from sys import argv
 from os.path import exists
 
-script, from_file, to_file = argv
+#script, from_file, to_file = argv
 
-print(f"Copying from {from_file} to {to_file}")
+#print(f"Copying from {argv[1]} to {argv[2]}")
 
-in_file = open(from_file)
-indata = in_file.read()
+#in_file = open(argv[1])
+#indata = in_file.read()
 
-print(f"The input file is {len(indata)} bytes long")
+with open(argv[1]) as in_file:
+    with open(argv[2], 'w') as out_file:
+        out_file.write(in_file.read())
 
-print(f"Does the output file exist?  {exists(to_file)}")
-print("Ready, hit RETURN to continue, CTRL-C to abort.")
-input()
+#print(f"The input file is {len(indata)} bytes long")
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+#print(f"Does the output file exist?  {exists(to_file)}")
+#print("Ready, hit RETURN to continue, CTRL-C to abort.")
+# input()
 
-print(f"Alright, all done.")
+#out_file = open(argv[2], 'w')
+# out_file.write(indata)
 
-out_file.close()
-in_file.close()
+#print(f"Alright, all done.")
+
+# out_file.close()
+# in_file.close()
