6480a0e41b604adfff4b1edef6711607b62949b4
Kevin J Donohue <kevinjdonohue@gmail.com>
2019-10-28 17:32:04 -0700

    Adding exercise 39 -- incomplete, just the first part

--- ex39.py
+++ ex39.py
@@ -1,1 +1,29 @@
 """Exercise 39."""
+
+states = {
+    "Oregon": "OR",
+    "Florida": "FL",
+    "California": "CA",
+    "New York": "NY",
+    "Michigan": "MI"
+}
+
+cities = {
+    "CA": "San Francisco",
+    "MI": "Detroit",
+    "FL": "Jacksonville"
+}
+
+cities["NY"] = "New York"
+cities["OR"] = "Portland"
+
+print("-" * 10)
+
+state_formatter = "{} State has: {}"
+
+print(state_formatter.format("NY", cities["NY"]))
+print(state_formatter.format("OR", cities["OR"]))
+
+print("-" * 10)
+
+# city_state_formatter = "{}"
